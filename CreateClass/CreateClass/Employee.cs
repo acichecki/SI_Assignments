95aa0ddd0a52af6447974748dfa803e290a98538
acichecki <a.cichecki@gmail.com>
2019-01-28 12:41:03 +0100

    Exercise 2: Employee class

---
+++ CreateClass/CreateClass/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Employee : Person
+    {
+        public int Salary { get; set; }
+        public string Profession { get; set; }
+        public Room RoomNumber { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + ". Salary: " + Salary + ". Profession: " + Profession + ". Works in room nr " + RoomNumber.RoomNumber;
+        }
+
+        public class Room
+        {
+            public int RoomNumber { get; set; }
+        }
+    }
+}
--- CreateClass/CreateClass/Program.cs
+++ CreateClass/CreateClass/Program.cs
@@ -11,7 +11,17 @@ namespace CreateClass
         static void Main(string[] args)
         {
             Person Krzysio = new Person() { Name = "Krzysio", BirthDate = new DateTime(1989, 3, 14), Gender = Person.Genders.Male};
-            Console.Write(Krzysio.ToString());
+            Employee Krzyslav = new Employee()
+            {
+                Name = "Krzyslav",
+                BirthDate = new DateTime(1989, 3, 14),
+                Gender = Person.Genders.Male,
+                Salary = 3000,
+                Profession = "JS Dev",
+                RoomNumber = new Employee.Room() {RoomNumber = 5}
+            };
+            Console.WriteLine(Krzysio.ToString());
+            Console.Write(Krzyslav.ToString());
             Console.ReadLine();
         }
     }
