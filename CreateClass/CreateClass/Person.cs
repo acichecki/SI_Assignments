e556d91088482574173705a54cf97bd10bbafe8a
acichecki <a.cichecki@gmail.com>
2019-01-28 12:24:13 +0100

    Person class with properties: Name, BirthDate and Gender.

---
+++ CreateClass/CreateClass/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Genders Gender { get; set; }
+
+        public enum Genders
+        {
+            Male,
+            Female
+        }
+
+        public override string ToString()
+        {
+            return this.Name + " was born on " + this.BirthDate.Day + "." + this.BirthDate.Month + "." + this.BirthDate.Year + " and is " + this.Gender;
+        }
+    }
+}
---
+++ CreateClass/CreateClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Krzysio = new Person() { Name = "Krzysio", BirthDate = new DateTime(1989, 3, 14), Gender = Person.Genders.Male};
+            Console.Write(Krzysio.ToString());
+            Console.ReadLine();
+        }
+    }
+}
