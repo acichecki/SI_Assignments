4b508609d285163706891302941f086c09457762
acichecki <a.cichecki@gmail.com>
2019-01-29 20:50:52 +0100

    Exercise 4

---
+++ LookupCollections/LookupCollections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using  System.Collections.Specialized;
+using  System.Globalization;
+
+namespace LookupCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListDictionary listDictionary = new ListDictionary(StringComparer.InvariantCultureIgnoreCase);
+            listDictionary.Add("United States", "Estados Unidos");
+            listDictionary.Add("Canada", "Canadá");
+            listDictionary.Add("Spain", "España");
+
+            Console.WriteLine(listDictionary["spain"]);
+            Console.WriteLine(listDictionary["canada"]);
+
+            Console.ReadKey();
+        }
+    }
+}
