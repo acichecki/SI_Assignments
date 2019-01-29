6d2916d073a755bd8ee17ca48249cf2c661a6a8e
acichecki <a.cichecki@gmail.com>
2019-01-29 20:38:47 +0100

    Exercise 3

---
+++ DictionaryCollections/DictionaryCollections/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+namespace DictionaryCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("0", "zero");
+            hashtable.Add("1", "one");
+            hashtable.Add("2", "two");
+            hashtable.Add("3", "three");
+            hashtable.Add("4", "four");
+            hashtable.Add("5", "five");
+            hashtable.Add("6", "six");
+            hashtable.Add("7", "seven");
+            hashtable.Add("8", "eight");
+            hashtable.Add("9", "nine");
+
+            string someString = "032134512387";
+
+            foreach (var number in someString)
+            {
+                Console.WriteLine(hashtable[number.ToString()]);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
