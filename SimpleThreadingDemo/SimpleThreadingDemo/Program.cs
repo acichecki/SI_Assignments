4623a245af10b52fd6437719d6cb9a4b02518f10
acichecki <a.cichecki@gmail.com>
2019-01-30 17:39:47 +0100

    Exercise 1

---
+++ SimpleThreadingDemo/SimpleThreadingDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SimpleThreadingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadOne = new Thread(new ThreadStart(Counting));
+            Thread threadTwo = new Thread(new ThreadStart(Counting));
+            threadOne.Start();
+            threadTwo.Start();
+            threadOne.Join();
+            threadTwo.Join();
+            Console.ReadKey();
+        }
+
+        static void Counting()
+        {
+            for(int i=1; i<=10 ; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
