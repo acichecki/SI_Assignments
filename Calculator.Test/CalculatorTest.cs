d10d8204201ea1d761f5e2e926a178c50d290154
acichecki <a.cichecki@gmail.com>
2019-01-30 17:25:06 +0100

    NUnit

---
+++ Calculator.Test/CalculatorTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test
+{
+    [TestFixture]
+    class CalculatorTest
+    {
+        private ICalculator sut;
+
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+            sut = new Calculator();
+        }
+
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            int expectedResult = sut.Add(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(15));
+        }
+
+        [Test]
+        public void ShouldMulTwoNumbers()
+        {
+            int expectedResult = sut.Mul(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(56));
+        }
+
+        [OneTimeTearDown]
+        public void TestTearDown()
+        {
+            sut = null;
+        }
+    
+    }
+}
