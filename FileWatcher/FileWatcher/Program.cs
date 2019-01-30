6f1f33c36a05390e307b123a3bd79b65fa1c0cd9
acichecki <a.cichecki@gmail.com>
2019-01-30 14:28:34 +0100

    FileWatcher

---
+++ FileWatcher/FileWatcher/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Security.Permissions;
+
+namespace FileWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+            Console.ReadKey();
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        private static void Run()
+        {
+            string[] args = System.Environment.GetCommandLineArgs();
+
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: Watcher.exe (directory)");
+                return;
+            }
+
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = args[1];
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName |
+                                   NotifyFilters.DirectoryName;
+            watcher.Filter = "*.txt";
+
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            while (Console.Read() != 'q') ;
+
+        }
+
+        private static void OnRenamed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File renamed to {0}", e.FullPath);
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+        }
+    }
+}
