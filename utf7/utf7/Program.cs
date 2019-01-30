d81e5ce52b8dd13669ff68d4a5f65712dbf73750
acichecki <a.cichecki@gmail.com>
2019-01-30 14:10:34 +0100

    Utf7

---
+++ utf7/utf7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utf7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding u7 = Encoding.UTF7;
+
+            string path = @"C:\Users\adam\Desktop\dotNet\SI_Assignments\";
+
+            string gitIgnore = File.ReadAllText(path + ".gitignore");
+
+            Console.WriteLine(gitIgnore);
+
+            byte[] bytes = u7.GetBytes(gitIgnore);
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, "gitignore_utf7.txt")))
+            {
+                foreach (var line in bytes)
+                    outputFile.WriteLine(line);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
